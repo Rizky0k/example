5fe6a7eb1b147e77f05079f457a0692c04f4f702
Rizky Hafizh Jatmiko <Rizky0409j@gmail.com>
2024-10-08 12:20:54 +0700

    error type

--- src/utils/mdFormatter.ts
+++ src/utils/mdFormatter.ts
@@ -1,26 +1,26 @@
 import moment from "moment";
 
-export const preprocessMarkdown = (markdown) => {
+export const preprocessMarkdown = (markdown: string) => {
   return markdown
     .split("\n\n")
     .filter((block) => !block.trim().startsWith("!["))
     .join("\n\n");
 };
 
-export const formatDate = (isoDateString) => {
+export const formatDate = (isoDateString: string) => {
   const formattedDate = moment(isoDateString).format("MMMM DD, YYYY");
   return formattedDate;
 };
-export const countDown = (isoDateString) => {
+export const countDown = (isoDateString: string) => {
   const formattedDate = moment(isoDateString).startOf("hour").fromNow();
   return formattedDate;
 };
-export const formatDateDetail = (isoDateString) => {
+export const formatDateDetail = (isoDateString: string) => {
   const formattedDate = moment(isoDateString).format("MMMM DD, YYYY HH:MM:SS");
   return formattedDate;
 };
 
-export const formatDateMonthDay = (isoDateString) => {
+export const formatDateMonthDay = (isoDateString: string) => {
   const formattedDate = moment(isoDateString).format("MMMM DD");
   return formattedDate;
 };
